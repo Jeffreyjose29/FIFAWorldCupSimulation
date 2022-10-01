4462681fbecda3e22f39790beb0c2275c8aed8e8
Jeffrey Jpse <jeffreyjose91@gmail.com>
2022-10-02 12:41:37 +1300

    Fixed the matrix for countries with same name and fixed players preferred position

--- R/01 Data Extraction.R
+++ R/01 Data Extraction.R
@@ -1,5 +1,5 @@
 # Package names
-packages <- c("dplyr", "ggplot2", "rvest", "data.table", "qdapRegex", "stringr", "htmltab", "tidyr")
+packages <- c("dplyr", "ggplot2", "rvest", "data.table", "qdapRegex", "stringr", "htmltab", "tidyr", "RSelenium")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -214,6 +214,51 @@ matrix_df <- subset(matrix_df, rownames(matrix_df) %in% c("Qatar", "Ecuador", "S
 matrix_df <- matrix_df[, worldCupTeams]
 matrix_df <- matrix_df[, order(colnames(matrix_df))]
 
+# Countries that start with the same letters do not seem to work so I have manually encoded it
+# Row : Column : Value To Input
+# 
+# Australia : Argentina : 0.17
+# Brazil : Belgium : 0.60
+# Canada : Cameroon : 0.00
+# CostaRica : Cameroon : 1.00
+# Croatia : Cameroon : 1.00
+# CostaRica : Canada : 0.60
+# England : Ecuador : 1.00
+# Ghana : Germany : 0.00
+# Portugal : Poland : 0.77
+# Senegal : SaudiArabia : 0.00
+# SouthKorea : SaudiArabia : 0.44
+# Spain : SaudiArabia : 1.00
+# SouthKorea : Senegal : 0.25
+# SouthKorea : Serbia : 0.40
+# Spain : Serbia : 1.00
+# Switzerland : Serbia : 0.50
+# Spain : SouthKorea : 0.80
+# Switzerland : SouthKorea : 0.50
+# Switzerland : Spain : 0.06
+# Uruguay : UnitedStates : 0.33
+
+matrix_df["Australia", "Argentina"] <- round(0.17, 2)
+matrix_df["Brazil", "Belgium"] <- round(0.60, 2)
+matrix_df["Canada", "Cameroon"] <- round(0.00, 2)
+matrix_df["CostaRica", "Cameroon"] <- round(1.00, 2)
+matrix_df["Croatia", "Cameroon"] <- round(1.00, 2)
+matrix_df["CostaRica", "Canada"] <- round(0.60, 2)
+matrix_df["England", "Ecuador"] <- round(1.00, 2)
+matrix_df["Ghana", "Germany"] <- round(0.00, 2)
+matrix_df["Portugal", "Poland"] <- round(0.77, 2)
+matrix_df["Senegal", "SaudiArabia"] <- round(0.00, 2)
+matrix_df["SouthKorea", "SaudiArabia"] <- round(0.44, 2)
+matrix_df["Spain", "SaudiArabia"] <- round(1.00, 2)
+matrix_df["SouthKorea", "Senegal"] <- round(0.25, 2)
+matrix_df["SouthKorea", "Serbia"] <- round(0.40, 2)
+matrix_df["Spain", "Serbia"] <- round(1.00, 2)
+matrix_df["Switzerland", "Serbia"] <- round(0.50, 2)
+matrix_df["Spain", "SouthKorea"] <- round(0.80, 2)
+matrix_df["Switzerland", "SouthKorea"] <- round(0.50, 2)
+matrix_df["Switzerland", "Spain"] <- round(0.06, 2)
+matrix_df["Uruguay", "UnitedStates"] <- round(0.33, 2)
+
 
 # Step 2: Extract Player Data
 players_df <- data.frame()
@@ -227,6 +272,37 @@ for(i in 1:570){
   players_df <- rbind(players_df, players_placeholder)
 }
 
+
+################## Trialing Data Extraction
+link <- "https://www.fifaindex.com/players/fifa23_552/?page=1"
+driver <- read_html(link)
+tableIndex <- html_nodes(driver, css = "table")
+tables <- html_nodes(driver, css = "table")
+players_placeholder <- html_table(tables)[[1]]
+
+test <- link %>% read_html %>%
+  html_node(xpath = '/html/body/main/div/div/div[2]/div[3]/table/tbody/tr[1]/td[2]/a') %>%
+  html_attr(., "title")
+
+federationPage <- read_html("https://en.wikipedia.org/wiki/Confederation_of_African_Football")
+CAF <- federationPage %>%
+  html_node(xpath = '/html/body/div[3]/div[3]/div[5]/div[1]/table[4]')  %>% html_table()
+CAF$Federation <- "CAF"
+colnames(CAF)[1] <- "Abbreviation"
+CAF <- CAF %>%
+  select(Abbreviation, Association, `IOC member`, Federation) %>%
+  filter(!grepl(')', Abbreviation))
+
+
+"https://www.fifaindex.com/players/fifa23_552/?page=1" %>% 
+  read_html() %>% 
+  html_nodes(":contains('link-nation')") %>% # search for string. Note the separate types of quotations
+  xml2::xml_attrs() %>% # show all the attributes the string belongs to
+  purrr::map("class") %>% # pull just 'class' attrs from the list
+  unlist %>% unique
+
+
+###########################################
 players_df <- players_df %>%
   drop_na(`OVR-POT`) %>%
   select(`OVR-POT`, Name, `Preferred Positions`, Age) %>%
@@ -236,56 +312,31 @@ players_df$Overall <- substr(players_df$Overall, start = 1, stop = 2)
 
 playingPositions <- c("GK", "SW", "RWB", "RB", "CB", "LWB", "LB", "CDM", "CM", "RM", "LM", "CAM", "CF", "RW", "LW", "ST")
 
-players_df$GK <- ifelse(grepl("GK", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "GK", NA)
-players_df$SW <- ifelse(grepl("SW", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "SW", NA)
-players_df$RWB <- ifelse(grepl("RWB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RWB", NA)
-players_df$RB <- ifelse(grepl("RB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RB", NA)
-players_df$CB <- ifelse(grepl("CB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CB", NA)
-players_df$LWB <- ifelse(grepl("LWB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LWB", NA)
-players_df$LB <- ifelse(grepl("LB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LB", NA)
-players_df$CDM <- ifelse(grepl("CDM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CDM", NA)
-players_df$CM <- ifelse(grepl("CM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CM", NA)
-players_df$RM <- ifelse(grepl("RM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RM", NA)
-players_df$LM <- ifelse(grepl("LM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LM", NA)
-players_df$CAM <- ifelse(grepl("CAM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CAM", NA)
-players_df$CF <- ifelse(grepl("CF", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CF", NA)
-players_df$RW <- ifelse(grepl("RW", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RW", NA)
-players_df$LW <- ifelse(grepl("LW", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LW", NA)
-players_df$ST <- ifelse(grepl("ST", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "ST", NA)
+players_df$GK <- ifelse(grepl("GK", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "GK,", NA)
+players_df$SW <- ifelse(grepl("SW", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "SW,", NA)
+players_df$RWB <- ifelse(grepl("RWB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RWB,", NA)
+players_df$RB <- ifelse(grepl("RB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RB,", NA)
+players_df$CB <- ifelse(grepl("CB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CB,", NA)
+players_df$LWB <- ifelse(grepl("LWB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LWB,", NA)
+players_df$LB <- ifelse(grepl("LB", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LB,", NA)
+players_df$CDM <- ifelse(grepl("CDM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CDM,", NA)
+players_df$CM <- ifelse(grepl("CM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CM,", NA)
+players_df$RM <- ifelse(grepl("RM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RM,", NA)
+players_df$LM <- ifelse(grepl("LM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LM,", NA)
+players_df$CAM <- ifelse(grepl("CAM", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CAM,", NA)
+players_df$CF <- ifelse(grepl("CF", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "CF,", NA)
+players_df$RW <- ifelse(grepl("RW", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "RW,", NA)
+players_df$LW <- ifelse(grepl("LW", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "LW,", NA)
+players_df$ST <- ifelse(grepl("ST", players_df$`Preferred Positions`, fixed = TRUE) == TRUE, "ST,", NA)
 
+players_df <- players_df  %>%
+  mutate_all(~str_replace_na(., "")) %>%
+  mutate(`Preferred Positions` = paste0(GK, SW, RWB, RB, CB, LWB, LB, CDM, CM, RM, LM, 
+                            CAM, CF, RW, LW, ST))
+players_df$`Preferred Positions` <- substr(players_df$`Preferred Positions`,1,nchar(players_df$`Preferred Positions`)-1)
 
-players_df$Position <- paste(players_df$GK, ",",
-                              players_df$SW, ",",
-                              players_df$RWB, ",",
-                              players_df$RB, ",",
-                              players_df$CB, ",",
-                              players_df$LWB, ",",
-                              players_df$LB, ",",
-                              players_df$CDM, ",",
-                              players_df$CM, ",",
-                              players_df$RM, ",",
-                              players_df$LM, ",",
-                              players_df$CAM, ",",
-                              players_df$CF, ",",
-                              players_df$RW, ",",
-                              players_df$LW, ",",
-                              players_df$ST, ",",
-                             collapse = ' ',
-                             na.skip = TRUE)
-
-
-for (i in 1:length(players_df$`Preferred Positions`)) {
-  string <- players_df$`Preferred Positions`[i]
-  findWords <- intersect(unlist(strsplit(string," ")),playingPositions)
-  if (!is.null(findWords)) {
-    for (j in findWords) {
-      players_df$`Preferred Positions`[i] <- gsub(j,paste0(j,","),string)
-    }
-  }
-}
-
-
-
+players_df <- players_df %>%
+  select(Overall, Name, `Preferred Positions`, Age)
 
 # Step 4: Write out the extracted datasets to a .csv file for back-up
 write.csv(federations, "Federations.csv", row.names = FALSE)
