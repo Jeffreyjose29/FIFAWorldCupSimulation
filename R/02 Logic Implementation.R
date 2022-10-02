567abb4c23dc3fec6ccca94932a1a8ae7a15e263
Jeffrey Jpse <jeffreyjose91@gmail.com>
2022-10-02 20:47:22 +1300

    Started The Second Script

--- R/02 Logic Implementation.R
+++ R/02 Logic Implementation.R
@@ -3,3 +3,6 @@
 ## Date: 2nd October 2022
 ###################################################
 
+playerNationality <- players_df$Nationality %>% table() %>% as.matrix()
+playerNationality <- data.frame(Nation = rownames(playerNationality), Player = playerNationality) %>% as_tibble()
+
